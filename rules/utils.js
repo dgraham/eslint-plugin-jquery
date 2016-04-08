04d5a989d835a5db1c144a0f879cd5ff6f16c1f7
David Graham <david.malcom.graham@gmail.com>
2016-04-08 17:42:27 -0600

    Add isjQuery function documentation

--- rules/utils.js
+++ rules/utils.js
@@ -17,6 +17,17 @@ function traverse(node) {
   }
 }
 
+// Traverses from a node up to its root parent to determine if it
+// originated from a jQuery `$()` function.
+//
+// node - The CallExpression node to start the traversal.
+//
+// Examples
+//
+//   // $('div').find('p').first()
+//   isjQuery(firstNode) // => true
+//
+// Returns true if the function call node is attached to a jQuery element set.
 function isjQuery(node) {
   const id = traverse(node)
   return id && id.name.startsWith('$')
