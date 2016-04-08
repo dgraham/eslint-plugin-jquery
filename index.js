a98ecbc8228b3c2842b06513e7cc04a4690d7401
David Graham <david.malcom.graham@gmail.com>
2016-04-08 15:25:58 -0600

    Add no-closest rule

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ module.exports = {
   rules: {
     'no-ajax': require('./rules/no-ajax'),
     'no-ajax': require('./rules/no-class'),
+    'no-closest': require('./rules/no-closest'),
     'no-each': require('./rules/no-each'),
     'no-map': require('./rules/no-map'),
     'no-trim': require('./rules/no-trim'),
---
+++ tests/no-closest.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const rule = require('../rules/no-closest')
+const RuleTester = require('eslint').RuleTester
+
+const error = '$.closest is not allowed'
+
+const ruleTester = new RuleTester()
+ruleTester.run('no-closest', rule, {
+  valid: [
+    'closest()',
+    '[].closest()',
+    'div.closest()',
+  ],
+  invalid: [
+    {
+      code: '$("div").closest()',
+      errors: [{message: error, type: 'CallExpression'}]
+    },
+    {
+      code: '$div.closest()',
+      errors: [{message: error, type: 'CallExpression'}]
+    },
+    {
+      code: '$("div").first().closest()',
+      errors: [{message: error, type: 'CallExpression'}]
+    },
+    {
+      code: '$("div").append($("input").closest())',
+      errors: [{message: error, type: 'CallExpression'}]
+    }
+  ]
+})
