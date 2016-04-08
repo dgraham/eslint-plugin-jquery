fdb5f184aff4f3bb75824e2a2a6162d60bf32bad
David Graham <david.malcom.graham@gmail.com>
2016-04-08 15:37:03 -0600

    Add no-parent rule

---
+++ tests/no-parent.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const rule = require('../rules/no-parent')
+const RuleTester = require('eslint').RuleTester
+
+const error = '$.parent is not allowed'
+
+const ruleTester = new RuleTester()
+ruleTester.run('no-parent', rule, {
+  valid: [
+    'parent()',
+    '[].parent()',
+    'div.parent()',
+    'div.parent'
+  ],
+  invalid: [
+    {
+      code: '$("div").parent()',
+      errors: [{message: error, type: 'CallExpression'}]
+    },
+    {
+      code: '$div.parent()',
+      errors: [{message: error, type: 'CallExpression'}]
+    },
+    {
+      code: '$("div").first().parent()',
+      errors: [{message: error, type: 'CallExpression'}]
+    },
+    {
+      code: '$("div").append($("input").parent())',
+      errors: [{message: error, type: 'CallExpression'}]
+    }
+  ]
+})
