b3c813b5fc6a881009c15f503a0be2ff65c6d1c5
David Graham <david.malcom.graham@gmail.com>
2016-04-09 09:20:11 -0600

    Add no-global-eval rule

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ module.exports = {
     'no-deferred': require('./rules/no-deferred'),
     'no-each': require('./rules/no-each'),
     'no-find': require('./rules/no-find'),
+    'no-global-eval': require('./rules/no-global-eval'),
     'no-html': require('./rules/no-html'),
     'no-inarray': require('./rules/no-inarray'),
     'no-map': require('./rules/no-map'),
