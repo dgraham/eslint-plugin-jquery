9cc6bb5fcdcbaaec7d0713b4885b0be34db0b8b1
David Graham <david.malcom.graham@gmail.com>
2016-04-09 09:35:46 -0600

    Add no-bind rule

---
+++ rules/no-bind.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const utils = require('./utils.js')
+
+module.exports = function(context) {
+  return {
+    CallExpression: function(node) {
+      if (node.callee.type !== 'MemberExpression') return
+      if (node.callee.property.name !== 'bind') return
+
+      if (utils.isjQuery(node)) {
+        context.report({
+          node: node,
+          message: '$.bind is not allowed'
+        })
+      }
+    }
+  }
+}
+
+module.exports.schema = []
---
+++ tests/no-bind.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const rule = require('../rules/no-bind')
+const RuleTester = require('eslint').RuleTester
+
+const error = '$.bind is not allowed'
+
+const ruleTester = new RuleTester()
+ruleTester.run('no-bind', rule, {
+  valid: [
+    'bind()',
+    '[].bind()',
+    'div.bind()',
+    'div.bind'
+  ],
+  invalid: [
+    {
+      code: '$("div").bind()',
+      errors: [{message: error, type: 'CallExpression'}]
+    },
+    {
+      code: '$div.bind()',
+      errors: [{message: error, type: 'CallExpression'}]
+    },
+    {
+      code: '$("div").first().bind()',
+      errors: [{message: error, type: 'CallExpression'}]
+    },
+    {
+      code: '$("div").append($("input").bind())',
+      errors: [{message: error, type: 'CallExpression'}]
+    }
+  ]
+})
