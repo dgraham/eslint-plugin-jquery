5f9e0f1bc828a04eef471010da2822fce25b41af
David Graham <david.malcom.graham@gmail.com>
2016-05-21 19:57:55 -0600

    Prevent only $ name usage

--- rules/no-proxy.js
+++ rules/no-proxy.js
@@ -4,7 +4,7 @@ module.exports = function(context) {
   return {
     CallExpression: function(node) {
       if (node.callee.type !== 'MemberExpression') return
-      if (node.callee.object.name !== '$' && node.callee.object.name !== 'jQuery') return
+      if (node.callee.object.name !== '$') return
       if (node.callee.property.name !== 'proxy') return
 
       context.report({
