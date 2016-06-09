3c983194f2722e6531aaf03c7ab248ad351a38d6
David Graham <david.malcom.graham@gmail.com>
2016-06-09 16:50:17 -0600

    Add no-fade rule

---
+++ rules/no-fade.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const utils = require('./utils.js')
+
+module.exports = function(context) {
+  const forbidden = ['fadeIn', 'fadeOut', 'fadeTo', 'fadeToggle']
+
+  return {
+    CallExpression: function(node) {
+      if (node.callee.type !== 'MemberExpression') return
+      if (forbidden.indexOf(node.callee.property.name) === -1) return
+
+      if (utils.isjQuery(node)) {
+        context.report({
+          node: node,
+          message: '$.' + node.callee.property.name + ' is not allowed'
+        })
+      }
+    }
+  }
+}
+
+module.exports.schema = []
